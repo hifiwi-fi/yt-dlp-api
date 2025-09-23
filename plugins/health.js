992151fe32cf9559c5739807df9aee7a3e4eba32
Bret Comnes <bcomnes@gmail.com>
2025-09-22 17:56:45 -0700

    Quiet down health logging

--- plugins/health.js
+++ plugins/health.js
@@ -6,7 +6,7 @@ import fp from 'fastify-plugin'
  * @see https://github.com/smartiniOnGitHub/fastify-healthcheck
  */
 export default fp(async function (fastify, _opts) {
-  fastify.register(import('fastify-healthcheck'))
+  fastify.register(import('fastify-healthcheck'), { logLevel: 'debug' })
 }, {
   name: 'health',
 })
